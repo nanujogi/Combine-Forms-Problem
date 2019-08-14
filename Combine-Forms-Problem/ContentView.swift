43198ffff92d64d7e000350cdd0172a0ab48c193
Nanu Jogi <nanu.jogi@gmail.com>
2019-08-14 19:08:30 +0530

    first commit

---
+++ Combine-Forms-Problem/ContentView.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+import Combine
+
+struct ContentView : View {
+    
+    @ObservedObject var registrationModel = RegistrationModel()
+    
+    @State private var registrationButtonDisabled = true
+    
+    @State private var validatedEMail: String = ""
+    @State private var validatedPassword: String = ""
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Enter your EMail", text: $registrationModel.eMail)
+                SecureField("Enter a Password", text: $registrationModel.password)
+                SecureField("Enter the Password again", text: $registrationModel.passwordRepeat)
+                
+                Button(action: registrationButtonAction) {
+                    Text("Create Account")
+                }
+                .disabled($registrationButtonDisabled.value)
+                .onReceive(self.registrationModel.validatedCredentials) { newValidatedCredentials in
+                    print("🌼 newValidatedCredentials: \(String(describing: newValidatedCredentials))")
+                    self.$registrationButtonDisabled.value = (newValidatedCredentials == nil)
+                }
+            }
+            
+            Section {
+                Text("Validated EMail: \(validatedEMail)")
+                    .onReceive(self.registrationModel.validatedEMail) { newValidatedEMail in
+                        self.validatedEMail = newValidatedEMail != nil ? newValidatedEMail! : "EMail invalid"
+                }
+                
+                Text("Validated Password: \(validatedPassword)")
+                    .onReceive(self.registrationModel.validatedPassword) { newValidatedPassword in
+                        self.validatedPassword = newValidatedPassword != nil ? newValidatedPassword! : "Passwords to short or don't match"
+                }
+            }
+        }
+        .navigationBarTitle(Text("Sign Up"))
+    }
+    
+    func registrationButtonAction() {
+        print("Create Account Clicked")
+    }
+}
---
+++ Combine-Forms-Problem/RegistrationModel.swift
@@ -0,0 +1,69 @@
+import SwiftUI
+import Combine
+
+class RegistrationModel : ObservableObject {
+    
+    @Published var eMail: String = ""
+    @Published var password: String = ""
+    @Published var passwordRepeat: String = ""
+    
+    let q = DispatchQueue(label: "com.xyz.combine.experimenting")
+    
+    public var willChange = PassthroughSubject<Void, Never>()
+    
+    var validatedEMail: AnyPublisher<String?, Never> {
+        return $eMail
+//            .print("✏️ $eMail pipeline")
+//            .debounce(for: 0.5, scheduler: q)
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .removeDuplicates()
+            .map { username in
+                return Future { promise in
+                    print("username: \(username)")
+                    //DispatchQueue.main.async {
+                        self.usernameAvailable(username) { available in
+                            print("available: \(available)")
+                            promise(.success(available ? username : nil))
+                        }
+                    //}
+                }
+        }
+        .switchToLatest()
+        .eraseToAnyPublisher()
+    }
+    
+    var validatedPassword: AnyPublisher<String?, Never> {
+        return Publishers.CombineLatest($password, $passwordRepeat)
+//            .subscribe(on: RunLoop.main)
+//            .receive(on: RunLoop.main)
+            .debounce(for: 0.5, scheduler: RunLoop.main)
+            .map { password, passwordRepeat in
+                print("password: \(password), passwordRepeat: \(passwordRepeat)")
+                guard password == passwordRepeat, password.count > 5 else { return nil }
+
+                return password
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    var validatedCredentials: AnyPublisher<(String, String)?, Never> {
+        return Publishers.CombineLatest(validatedEMail, validatedPassword)
+            
+            .receive(on: RunLoop.main) // run on main thread
+            .map { validatedEMail, validatedPassword in
+                print("validatedEMail: \(validatedEMail ?? "not set"), validatedPassword: \(validatedPassword ?? "not set")")
+             
+                guard let eMail = validatedEMail, let password = validatedPassword else { return nil }
+                
+                return (eMail, password)
+                
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func usernameAvailable(_ username: String, completion: (Bool) -> Void) {
+        let isValidEMailAddress: Bool = NSPredicate(format:"SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}").evaluate(with: username)
+        
+        completion(isValidEMailAddress)
+    }
+}
