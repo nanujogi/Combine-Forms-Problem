53a406a9ce4af1fa867628c37786a92b2ec7102f
Nanu Jogi <nanu.jogi@gmail.com>
2019-09-18 17:12:44 +0530

    Edit button placed inside navigationBarItems

--- Combine-Forms-Problem/AllUsers.swift
+++ Combine-Forms-Problem/AllUsers.swift
@@ -12,35 +12,36 @@ struct AllUsers: View {
     
     @Environment(\.managedObjectContext) var managedObjectContext
     @FetchRequest(fetchRequest: User.alldataFetchRequest()) var allmyusers: FetchedResults<User>
-
+    
     var body: some View {
         NavigationView {
-        //Section(header: Text("User from CoreData")) {
-                List {
-                    ForEach(self.allmyusers) { myuser in
-                        VStack(alignment: .leading) {
-                            Text(myuser.usremail ?? "")
-                                .font(.headline)
-                            Text(myuser.usrpassword ?? "")
-                                .font(.headline)
-                            Text(myuser.appname ?? "")
-                                .font(.subheadline)
-                            Text(myuser.id ?? "")
-                                .font(.system(.caption))
-                        }
+            //Section(header: Text("User from CoreData")) {
+            List {
+                ForEach(self.allmyusers) { myuser in
+                    VStack(alignment: .leading) {
+                        Text(myuser.usremail ?? "")
+                            .font(.headline)
+                        Text(myuser.usrpassword ?? "")
+                            .font(.headline)
+                        Text(myuser.appname ?? "")
+                            .font(.subheadline)
+                        Text(myuser.id ?? "")
+                            .font(.system(.caption))
                     }
-                .onDelete(perform: removeUser)
                 }
-        //}
-        .font(.headline)
-        .animation(.easeInOut)
-    }
-    .navigationBarTitle("Users")
-    .navigationBarItems(trailing: EditButton())
+                .onDelete(perform: removeUser)
+            }
+                //}
+                .font(.headline)
+                .animation(.easeInOut)
+                .navigationBarTitle("Users")
+                .navigationBarItems(trailing: EditButton())
+        }
+        
     }
     
     func removeUser(at offsets: IndexSet) {
-
+        
         for index in offsets {
             let useridx = allmyusers[index]
             managedObjectContext.delete(useridx)
